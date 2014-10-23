0afc68e0383fb7797f79b92d299c3041656107d4
Garry Smith <garrysmith@Lornas-MacBook-Air.local>
2014-10-23 13:18:12 +1100

    First Rev of Project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        
+        messageLabel.hidden=false
+        messageLabel.text=enterMessageTextField.text
+        enterMessageTextField.text=""
+        
+        //uses the class function UIColor automatically instances object
+        messageLabel.textColor=UIColor.redColor()
+        
+        //Tells IOS that the TextField is no longer waiting for a response which disables the keyboard
+        enterMessageTextField.resignFirstResponder()
+        
+        // Sent the title and leave the focus unselected
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
 }
-
+}
